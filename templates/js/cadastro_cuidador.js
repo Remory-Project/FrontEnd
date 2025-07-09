c4823dfe63d973504d159ae45606c159387b3538
Pedr0 <elpedrorenan08@gmail.com>
2025-07-08 21:45:58 -0300

    integração do cadastro do cuidador

--- templates/js/cadastro_cuidador.js
+++ templates/js/cadastro_cuidador.js
@@ -1,31 +1,32 @@
-const form = document.getElementById('login-form');
+const form = document.getElementById('cadastro-form');
 const mensagem = document.getElementById('mensagem');
 
 form.addEventListener('submit', async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
+    const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
 
     try {
-        const resposta = await fetch('http://127.0.0.1:3333/login', {
+        const resposta = await fetch('http://127.0.0.1:3333/criar/cuidador', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email, password:senha }),
+            body: JSON.stringify({ nome, email, password_hash: senha }),
         });
 
         const dados = await resposta.json();
-        console.log("resposta", resposta.status)
+        console.log("resposta", resposta.status);
 
         if (resposta.ok) {
-            mensagem.textContent = dados.mensagem || 'Login realizado com sucesso!';
+            mensagem.textContent = dados.mensagem || 'Cadastro realizado com sucesso!';
             mensagem.style.color = 'green';
-            window.location.href = 'dashboard.html';
-
+            window.location.href = 'login.html';
+            
         } else {
-            mensagem.textContent = dados.mensagem || 'Erro no login.';
+            mensagem.textContent = dados.mensagem || 'Erro no cadastro.';
             mensagem.style.color = 'red';
         }
     } catch (erro) {
